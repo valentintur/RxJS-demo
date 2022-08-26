b2a768f01fcee10578f948f77fbdeb74853e87e4
Valentin Tur <valentin.tur.it@gmail.com>
2022-08-26 18:02:42 +0200

    feat: add setInterval functions

---
+++ src/problem.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById('interval')
+const rxjsBtn = document.getElementById('rxjs')
+const display = document.querySelector('#problem .result')
+
+const people = [
+  {name: 'Vladilen', age: 25},
+  {name: 'Elena', age: 17},
+  {name: 'Ivan', age: 18},
+  {name: 'Igor', age: 14},
+  {name: 'Lisa', age: 32},
+  {name: 'Irina', age: 23},
+  {name: 'Oleg', age: 20}
+]
+
+btn.addEventListener('click', () => {
+  btn.disabled = true;
+  let i = 0;
+  const canDrink = [];
+
+  const interval = setInterval(()=> {
+    
+    if (people[i]) {  
+      if (people[i].age >= 18) {
+        canDrink.push(people[i].name)
+      }
+      display.textContent = canDrink.join(' ')
+      i++
+    } else {
+      clearInterval(interval)
+      btn.disabled = false;
+    }
+
+  }, 1000)
+})
