5d688294bf2173354546115c302dbeb0dc738db3
Valentin Tur <valentin.tur.it@gmail.com>
2022-08-26 18:24:19 +0200

    feat: add RxJS interval

--- src/problem.js
+++ src/problem.js
@@ -1,3 +1,6 @@
+import {interval} from 'rxjs';
+import {filter, map, take, scan} from 'rxjs/operators';
+
 const btn = document.getElementById('interval')
 const rxjsBtn = document.getElementById('rxjs')
 const display = document.querySelector('#problem .result')
@@ -31,4 +34,19 @@ btn.addEventListener('click', () => {
     }
 
   }, 1000)
+})
+
+rxjsBtn.addEventListener('click', () => {
+  rxjsBtn.disabled = true;
+  interval(1000)
+    .pipe(
+      take(people.length),
+      filter(v => people[v].age >= 18),
+      map(v => people[v].name),
+      scan((acc, v)=> acc.concat(v), [])
+    )
+    .subscribe(res => {
+      display.textContent = res.join(' ')
+    }, null, () => rxjsBtn.disabled = false)
+
 })
